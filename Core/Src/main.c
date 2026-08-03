91ae9c31ed47a269901a62e09aa43693c0299062
johnnyeatworld89 <johnnyeatworld89@gmail.com>
2026-08-03 22:32:26 +0900

    Update main.c

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,137 +1,108 @@
 #include "main.h"
 
+/* Private variables ---------------------------------------------------------*/
 GPIO_InitTypeDef GPIO_InitStruct = {0};
 
-
+/* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void Error_Handler(void);
-
 
 int main(void)
 {
     HAL_Init();
-
     SystemClock_Config();
 
-
-    /* GPIO Clocks aktivieren */
+    /* GPIO Clocks */
     __HAL_RCC_GPIOA_CLK_ENABLE();
+    __HAL_RCC_GPIOB_CLK_ENABLE();
     __HAL_RCC_GPIOC_CLK_ENABLE();
 
-
-    /*
-     * LED Pin PA5 konfigurieren
-     */
+    /* ---------- Interne LED PA5 ---------- */
     GPIO_InitStruct.Pin = GPIO_PIN_5;
     GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
     GPIO_InitStruct.Pull = GPIO_NOPULL;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-
     HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
+    /* ---------- Externe LED PB0 ---------- */
+    GPIO_InitStruct.Pin = GPIO_PIN_0;
+    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
-    /*
-     * Schalter Pin PC13 konfigurieren
-     * Schalter gegen GND
-     */
+    /* LEDs aus */
+    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_RESET);
+    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0, GPIO_PIN_RESET);
+
+    /* ---------- Interner Button PC13 ---------- */
     GPIO_InitStruct.Pin = GPIO_PIN_13;
     GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
-    GPIO_InitStruct.Pull = GPIO_PULLUP;
-
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
     HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
+    /* ---------- Externer Schalter PB1 ---------- */
+    GPIO_InitStruct.Pin = GPIO_PIN_1;
+    GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+    GPIO_InitStruct.Pull = GPIO_PULLUP;
+    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
+    uint8_t intButtonOld = GPIO_PIN_SET;
+    uint8_t extButtonOld = GPIO_PIN_SET;
+
+    uint8_t intLedState = 0;
+    uint8_t extLedState = 0;
 
     while (1)
     {
+        /* ===== Interner Button =====
+           PC13: gedrückt = LOW
+        */
+        uint8_t intButton = HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13);
 
-        /*
-         * Schalter gedrückt:
-         * PC13 = LOW
-         */
-        if (HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13) == GPIO_PIN_RESET)
+        if ((intButtonOld == GPIO_PIN_SET) &&
+            (intButton == GPIO_PIN_RESET))
         {
-            /* LED EIN */
-            HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_SET);
-        }
-        else
-        {
-            /* LED AUS */
-            HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_RESET);
+            intLedState ^= 1;
+
+            HAL_GPIO_WritePin(GPIOA,
+                              GPIO_PIN_5,
+                              intLedState ? GPIO_PIN_SET : GPIO_PIN_RESET);
         }
 
+        intButtonOld = intButton;
+
+        /* ===== Externer Schalter =====
+           PB1: gedrückt = LOW
+        */
+        uint8_t extButton = HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_1);
+
+        if ((extButtonOld == GPIO_PIN_SET) &&
+            (extButton == GPIO_PIN_RESET))
+        {
+            extLedState ^= 1;
+
+            HAL_GPIO_WritePin(GPIOB,
+                              GPIO_PIN_0,
+                              extLedState ? GPIO_PIN_SET : GPIO_PIN_RESET);
+        }
+
+        extButtonOld = extButton;
+
+        /* Einfache Entprellung */
+        HAL_Delay(20);
     }
 }
 
-
-
+/**
+  * @brief System Clock Configuration
+  * (Deine bisherige Funktion unverändert übernehmen)
+  */
 void SystemClock_Config(void)
 {
-    RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
-    RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-
-
-    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
-    RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-    RCC_OscInitStruct.HSICalibrationValue =
-        RCC_HSICALIBRATION_DEFAULT;
-
-    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
-    RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL6;
-    RCC_OscInitStruct.PLL.PLLDIV = RCC_PLL_DIV3;
-
-
-    if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-    {
-        Error_Handler();
-    }
-
-
-    __HAL_RCC_PWR_CLK_ENABLE();
-
-    __HAL_PWR_VOLTAGESCALING_CONFIG(
-        PWR_REGULATOR_VOLTAGE_SCALE1);
-
-
-    while (__HAL_PWR_GET_FLAG(PWR_FLAG_VOS) != RESET)
-    {
-    }
-
-
-    RCC_ClkInitStruct.ClockType =
-        RCC_CLOCKTYPE_SYSCLK |
-        RCC_CLOCKTYPE_HCLK |
-        RCC_CLOCKTYPE_PCLK1 |
-        RCC_CLOCKTYPE_PCLK2;
-
-
-    RCC_ClkInitStruct.SYSCLKSource =
-        RCC_SYSCLKSOURCE_PLLCLK;
-
-    RCC_ClkInitStruct.AHBCLKDivider =
-        RCC_SYSCLK_DIV1;
-
-    RCC_ClkInitStruct.APB1CLKDivider =
-        RCC_HCLK_DIV1;
-
-    RCC_ClkInitStruct.APB2CLKDivider =
-        RCC_HCLK_DIV1;
-
-
-    if (HAL_RCC_ClockConfig(
-        &RCC_ClkInitStruct,
-        FLASH_LATENCY_1) != HAL_OK)
-    {
-        Error_Handler();
-    }
+    /* Deine vorhandene SystemClock_Config() hier unverändert einfügen */
 }
-
-
 
 static void Error_Handler(void)
 {
-    while(1)
+    while (1)
     {
     }
 }
