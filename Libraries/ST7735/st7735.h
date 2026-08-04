e8688845d9829540b42624e566291c5d4a2589fe
johnnyeatworld89 <johnnyeatworld89@gmail.com>
2026-08-04 16:33:21 +0900

    Update st7735.h

--- Libraries/ST7735/st7735.h
+++ Libraries/ST7735/st7735.h
@@ -1,1 +1,24 @@
+#ifndef ST7735_H
+#define ST7735_H
 
+#include "stm32l1xx_hal.h"
+
+// Displaygröße
+#define ST7735_WIDTH   128
+#define ST7735_HEIGHT  160
+
+// Farben RGB565
+#define ST7735_BLACK   0x0000
+#define ST7735_BLUE    0x001F
+#define ST7735_RED     0xF800
+#define ST7735_GREEN   0x07E0
+#define ST7735_WHITE   0xFFFF
+
+// SPI Handle kommt aus main.c
+extern SPI_HandleTypeDef hspi1;
+
+// API
+void ST7735_Init(void);
+void ST7735_FillScreen(uint16_t color);
+
+#endif
