20ea04b1b1e7beb64a263ad70af193aa1a26d074
johnnyeatworld89 <johnnyeatworld89@gmail.com>
2026-08-06 22:32:46 +0900

    Update ost4m18132a.c

--- Libraries/ost4m18132a/ost4m18132a.c
+++ Libraries/ost4m18132a/ost4m18132a.c
@@ -1,1 +1,25 @@
+#include "ost4ml8132a.h"
+
+#define LED_PORT GPIOA
+#define LED_PIN  GPIO_PIN_8
+
+static RGB_t leds[LED_COUNT];
+
+void LED_Init(void)
+{
+    GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+
+    GPIO_InitStruct.Pin = LED_PIN;
+    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+
+    HAL_GPIO_Init(LED_PORT, &GPIO_InitStruct);
+
+    HAL_GPIO_WritePin(LED_PORT,
+                      LED_PIN,
+                      GPIO_PIN_RESET);
+}
 
