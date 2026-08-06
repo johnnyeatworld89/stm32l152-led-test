ccc4daecbb9f0ee8c8b5183b0138dded5a1bed00
johnnyeatworld89 <johnnyeatworld89@gmail.com>
2026-08-06 22:48:05 +0900

    Update ost4m18132a.c

--- Libraries/ost4m18132a/ost4m18132a.c
+++ Libraries/ost4m18132a/ost4m18132a.c
@@ -23,3 +23,72 @@ void LED_Init(void)
                       GPIO_PIN_RESET);
 }
 
+static void LED_SendBit(uint8_t bit)
+{
+    if(bit)
+    {
+        HAL_GPIO_WritePin(LED_PORT,
+                          LED_PIN,
+                          GPIO_PIN_SET);
+
+        for(volatile int i=0;i<20;i++);
+
+        HAL_GPIO_WritePin(LED_PORT,
+                          LED_PIN,
+                          GPIO_PIN_RESET);
+
+        for(volatile int i=0;i<10;i++);
+    }
+    else
+    {
+        HAL_GPIO_WritePin(LED_PORT,
+                          LED_PIN,
+                          GPIO_PIN_SET);
+
+        for(volatile int i=0;i<8;i++);
+
+        HAL_GPIO_WritePin(LED_PORT,
+                          LED_PIN,
+                          GPIO_PIN_RESET);
+
+        for(volatile int i=0;i<20;i++);
+    }
+}
+static void LED_SendByte(uint8_t data)
+{
+    for(int i = 7; i >= 0; i--)
+    {
+        LED_SendBit((data >> i) & 0x01);
+    }
+}
+
+void LED_SetColor(uint8_t index,
+                  uint8_t r,
+                  uint8_t g,
+                  uint8_t b)
+{
+    if(index >= LED_COUNT)
+    {
+        return;
+    }
+
+    leds[index].r = r;
+    leds[index].g = g;
+    leds[index].b = b;
+}
+void LED_Show(void)
+{
+    __disable_irq();
+
+    for(uint8_t i = 0; i < LED_COUNT; i++)
+    {
+        LED_SendByte(leds[i].g);
+        LED_SendByte(leds[i].r);
+        LED_SendByte(leds[i].b);
+    }
+
+    __enable_irq();
+
+    HAL_Delay(1);
+}
+
