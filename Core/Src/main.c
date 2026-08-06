88e37e6c8a73e5826447d821ecaabcbbad298441
johnnyeatworld89 <johnnyeatworld89@gmail.com>
2026-08-06 21:37:11 +0900

    Update main.c

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,34 +1,214 @@
+@@ -1,21 +1,7 @@
+/**
+  ******************************************************************************
+  * @file    main.c
+  * @brief   STM32L152RE Nucleo - ST7735S SPI Display Test
+  ******************************************************************************
+  */
+
 #include "main.h"
 #include "st7735.h"
 
+/* Global SPI handle used by st7735.c */
+SPI_HandleTypeDef hspi1;
+
+/* Private function prototypes */
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_SPI1_Init(void);
+static void Error_Handler(void);
 extern SPI_HandleTypeDef hspi1;
 
 int main(void)
 {
-    HAL_Init();
-
-    SystemClock_Config();
-
+@@ -26,179 +12,23 @@ int main(void)
     MX_GPIO_Init();
     MX_SPI1_Init();
 
+    HAL_Delay(200);
+
     ST7735_Init();
 
+    ST7735_FillScreen(ST7735_BLACK);
+
+ST7735_DrawPixel(0, 0, ST7735_RED);
+
+ST7735_DrawPixel(
+    ST7735_WIDTH - 1,
+    0,
+    ST7735_GREEN);
+
+ST7735_DrawPixel(
+    0,
+    ST7735_HEIGHT - 1,
+    ST7735_BLUE);
+
+ST7735_DrawPixel(
+    ST7735_WIDTH - 1,
+    ST7735_HEIGHT - 1,
+    ST7735_WHITE);
+
+while (1)
+{
+}
+}
+
+/**
+  * @brief GPIO Initialization Function
+  *
+  * Display wiring:
+  * PA5 = SPI1_SCK
+  * PA7 = SPI1_MOSI
+  * PA4 = CS
+  * PB0 = DC/A0
+  * PB1 = RST
+  */
+static void MX_GPIO_Init(void)
+{
+    GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+    __HAL_RCC_GPIOB_CLK_ENABLE();
+
+    /* Default levels before enabling display */
+    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_SET);    /* CS high */
+    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0, GPIO_PIN_RESET);  /* DC low */
+    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_1, GPIO_PIN_SET);    /* RST high */
+
+    /* PA4 = CS */
+    GPIO_InitStruct.Pin = GPIO_PIN_4;
+    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+    /* PB0 = DC, PB1 = RST */
+    GPIO_InitStruct.Pin = GPIO_PIN_0 | GPIO_PIN_1;
+    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+    /* PA5 = SPI1_SCK, PA7 = SPI1_MOSI */
+    GPIO_InitStruct.Pin = GPIO_PIN_5 | GPIO_PIN_7;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+    GPIO_InitStruct.Alternate = GPIO_AF5_SPI1;
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+}
+
+/**
+  * @brief SPI1 Initialization Function
+  *
+  * ST7735S uses write-only SPI here:
+  * SCK  = PA5
+  * MOSI = PA7
+  * MISO unused
+  */
+static void MX_SPI1_Init(void)
+{
+    __HAL_RCC_SPI1_CLK_ENABLE();
+
+    hspi1.Instance = SPI1;
+    hspi1.Init.Mode = SPI_MODE_MASTER;
+    hspi1.Init.Direction = SPI_DIRECTION_1LINE;
+    hspi1.Init.DataSize = SPI_DATASIZE_8BIT;
+    hspi1.Init.CLKPolarity = SPI_POLARITY_LOW;
+    hspi1.Init.CLKPhase = SPI_PHASE_1EDGE;
+    hspi1.Init.NSS = SPI_NSS_SOFT;
+
+    /*
+      Start conservatively.
+      With 32 MHz system clock and prescaler 16:
+      SPI clock is about 2 MHz.
+      If the display works, we can later increase this.
+    */
+    hspi1.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_16;
+
+    hspi1.Init.FirstBit = SPI_FIRSTBIT_MSB;
+    hspi1.Init.TIMode = SPI_TIMODE_DISABLED;
+    hspi1.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLED;
+    hspi1.Init.CRCPolynomial = 7;
+
+    if (HAL_SPI_Init(&hspi1) != HAL_OK)
+    {
+        Error_Handler();
+    }
+}
+
+/**
+  * @brief System Clock Configuration
+  */
+void SystemClock_Config(void)
+{
+    RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+    RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+
+    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+    RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+    RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+
+    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+    RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL6;
+    RCC_OscInitStruct.PLL.PLLDIV = RCC_PLL_DIV3;
+
+    if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
     while (1)
     {
+        Error_Handler();
+    }
+
+    __HAL_RCC_PWR_CLK_ENABLE();
         ST7735_FillScreen(ST7735_RED);
         HAL_Delay(1000);
 
+    __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
         ST7735_FillScreen(ST7735_GREEN);
         HAL_Delay(1000);
 
+    while (__HAL_PWR_GET_FLAG(PWR_FLAG_VOS) != RESET)
+    {
+    }
         ST7735_FillScreen(ST7735_BLUE);
         HAL_Delay(1000);
 
+    RCC_ClkInitStruct.ClockType =
+        RCC_CLOCKTYPE_SYSCLK |
+        RCC_CLOCKTYPE_HCLK |
+        RCC_CLOCKTYPE_PCLK1 |
+        RCC_CLOCKTYPE_PCLK2;
         ST7735_FillScreen(ST7735_WHITE);
         HAL_Delay(1000);
 
+    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK)
+    {
+        Error_Handler();
         ST7735_FillScreen(ST7735_BLACK);
         HAL_Delay(1000);
     }
 }
+
+static void Error_Handler(void)
+{
+    while (1)
+    {
+    }
+}
+
+#ifdef USE_FULL_ASSERT
+
+void assert_failed(uint8_t *file, uint32_t line)
+{
+    while (1)
+    {
+    }
+}
+
+#endif
