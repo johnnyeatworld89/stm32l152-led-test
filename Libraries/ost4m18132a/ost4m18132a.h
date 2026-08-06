679a1ecbfc14ff5da16e843da6c37bad7f229bf1
johnnyeatworld89 <johnnyeatworld89@gmail.com>
2026-08-06 22:32:14 +0900

    Update ost4m18132a.h

--- Libraries/ost4m18132a/ost4m18132a.h
+++ Libraries/ost4m18132a/ost4m18132a.h
@@ -1,1 +1,25 @@
+#ifndef OST4ML8132A_H
+#define OST4ML8132A_H
+
+#include "stm32l1xx_hal.h"
+
+#define LED_COUNT 3
+
+typedef struct
+{
+    uint8_t r;
+    uint8_t g;
+    uint8_t b;
+} RGB_t;
+
+void LED_Init(void);
+
+void LED_SetColor(uint8_t index,
+                  uint8_t r,
+                  uint8_t g,
+                  uint8_t b);
+
+void LED_Show(void);
+
+#endif
 
