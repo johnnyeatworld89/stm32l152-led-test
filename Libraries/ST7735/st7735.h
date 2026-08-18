f837a358792cc1b7323568961f0c48955baf7f1e
johnnyeatworld89 <johnnyeatworld89@gmail.com>
2026-08-18 16:34:18 +0900

    Update st7735.h

--- Libraries/ST7735/st7735.h
+++ Libraries/ST7735/st7735.h
@@ -3,82 +3,113 @@
 
 #include "stm32l1xx_hal.h"
 
-/*
- * Physische Displaygröße
- *
- * Das Display besitzt 128 x 160 Pixel.
- */
-#define ST7735_PHYSICAL_WIDTH   128
-#define ST7735_PHYSICAL_HEIGHT  160
+
+/* -------------------------------------------------------------------------- */
+/* Display dimensions                                                         */
+/* -------------------------------------------------------------------------- */
+
+#define ST7735_WIDTH   128
+#define ST7735_HEIGHT  160
 
 
-/*
- * Aktuelle logische Displaygröße.
- *
- * Diese Werte werden durch ST7735_SetRotation()
- * automatisch angepasst.
- */
-extern uint16_t ST7735_WIDTH;
-extern uint16_t ST7735_HEIGHT;
+/* -------------------------------------------------------------------------- */
+/* Display offsets                                                            */
+/* -------------------------------------------------------------------------- */
+
+#define ST7735_XSTART  2
+#define ST7735_YSTART  1
 
 
-/* RGB565 Farben */
+/* -------------------------------------------------------------------------- */
+/* RGB565 colors                                                              */
+/* -------------------------------------------------------------------------- */
+
 #define ST7735_BLACK   0x0000
 #define ST7735_BLUE    0x001F
 #define ST7735_RED     0xF800
 #define ST7735_GREEN   0x07E0
 #define ST7735_WHITE   0xFFFF
+#define ST7735_YELLOW  0xFFE0
 #define ST7735_CYAN    0x07FF
 #define ST7735_MAGENTA 0xF81F
-#define ST7735_YELLOW  0xFFE0
 
 
-/* SPI Handle aus main.c */
+/* -------------------------------------------------------------------------- */
+/* SPI handle                                                                 */
+/* -------------------------------------------------------------------------- */
+
 extern SPI_HandleTypeDef hspi1;
 
 
-/* Initialisierung */
+/* -------------------------------------------------------------------------- */
+/* Initialization                                                             */
+/* -------------------------------------------------------------------------- */
+
 void ST7735_Init(void);
 
-
-/* Rotation */
 void ST7735_SetRotation(uint8_t rotation);
 
 
-/* Grundlegende Grafikfunktionen */
-void ST7735_FillScreen(uint16_t color);
+/* -------------------------------------------------------------------------- */
+/* Basic drawing                                                              */
+/* -------------------------------------------------------------------------- */
 
 void ST7735_DrawPixel(
     uint16_t x,
     uint16_t y,
-    uint16_t color);
+    uint16_t color
+);
+
+void ST7735_FillScreen(
+    uint16_t color
+);
+
+
+/* -------------------------------------------------------------------------- */
+/* Lines                                                                      */
+/* -------------------------------------------------------------------------- */
 
 void ST7735_DrawLine(
     int16_t x0,
     int16_t y0,
     int16_t x1,
     int16_t y1,
-    uint16_t color);
+    uint16_t color
+);
+
+
+/* -------------------------------------------------------------------------- */
+/* Rectangles                                                                 */
+/* -------------------------------------------------------------------------- */
 
 void ST7735_DrawRect(
     uint16_t x,
     uint16_t y,
-    uint16_t w,
-    uint16_t h,
-    uint16_t color);
+    uint16_t width,
+    uint16_t height,
+    uint16_t color
+);
 
 void ST7735_FillRect(
     uint16_t x,
     uint16_t y,
-    uint16_t w,
-    uint16_t h,
-    uint16_t color);
+    uint16_t width,
+    uint16_t height,
+    uint16_t color
+);
+
+
+/* -------------------------------------------------------------------------- */
+/* Arrows                                                                     */
+/* -------------------------------------------------------------------------- */
 
 void ST7735_DrawArrow(
     int16_t x0,
     int16_t y0,
     int16_t x1,
     int16_t y1,
-    uint16_t color);
+    uint16_t color
+);
+
 
 #endif
