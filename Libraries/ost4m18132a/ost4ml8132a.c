92100bcbd46c361f74f1d7891f1618a5eafebfcb
johnnyeatworld89 <johnnyeatworld89@gmail.com>
2026-08-26 11:31:57 +0900

    Update ost4ml8132a.c

--- Libraries/ost4m18132a/ost4ml8132a.c
+++ Libraries/ost4m18132a/ost4ml8132a.c
@@ -1,66 +1,80 @@
 #include "ost4ml8132a.h"
 
-#define LED_H_PORT GPIOA
-#define LED_H_PIN  GPIO_PIN_8
+#define DIN_H_PORT GPIOA
+#define DIN_H_PIN  GPIO_PIN_8
 
-#define LED_L_PORT GPIOA
-#define LED_L_PIN  GPIO_PIN_9
+#define DIN_L_PORT GPIOA
+#define DIN_L_PIN  GPIO_PIN_9
 
 static RGB_t leds[LED_COUNT];
 
-static void delay_us_soft(volatile uint32_t us)
+#define DIN_H_HIGH() (DIN_H_PORT->BSRR = DIN_H_PIN)
+#define DIN_H_LOW()  (DIN_H_PORT->BRR  = DIN_H_PIN)
+
+#define DIN_L_HIGH() (DIN_L_PORT->BSRR = DIN_L_PIN)
+#define DIN_L_LOW()  (DIN_L_PORT->BRR  = DIN_L_PIN)
+
+static void LED_DelayUs(uint32_t microseconds)
 {
-    while (us--)
+    /*
+     * Der Systemtakt beträgt 32 MHz.
+     * Diese Softwareverzögerung ist zunächst für den Funktionstest gedacht.
+     */
+    while (microseconds--)
     {
-        for (volatile uint32_t i = 0; i < 32; i++)
+        for (volatile uint32_t i = 0; i < 7; i++)
         {
             __NOP();
         }
     }
 }
 
-static void DIN_Middle(void)
+static void LED_Middle(void)
 {
-    HAL_GPIO_WritePin(LED_H_PORT, LED_H_PIN, GPIO_PIN_RESET);
-    HAL_GPIO_WritePin(LED_L_PORT, LED_L_PIN, GPIO_PIN_SET);
+    DIN_H_LOW();
+    DIN_L_HIGH();
 }
 
-static void SendHighBit(void)
+static void LED_HighBit(void)
 {
-    HAL_GPIO_WritePin(LED_H_PORT, LED_H_PIN, GPIO_PIN_SET);
-    HAL_GPIO_WritePin(LED_L_PORT, LED_L_PIN, GPIO_PIN_SET);
+    DIN_H_HIGH();
+    DIN_L_HIGH();
 
-    delay_us_soft(50);
+    LED_DelayUs(1);
 
-    DIN_Middle();
+    LED_Middle();
 
-    delay_us_soft(50);
+    LED_DelayUs(1);
 }
 
-static void SendLowBit(void)
+static void LED_LowBit(void)
 {
-    HAL_GPIO_WritePin(LED_H_PORT, LED_H_PIN, GPIO_PIN_RESET);
-    HAL_GPIO_WritePin(LED_L_PORT, LED_L_PIN, GPIO_PIN_RESET);
+    DIN_H_LOW();
+    DIN_L_LOW();
 
-    delay_us_soft(50);
+    LED_DelayUs(1);
 
-    DIN_Middle();
+    LED_Middle();
 
-    delay_us_soft(50);
+    LED_DelayUs(1);
 }
 
-static void LED_SendByte(uint8_t data)
+static void LED_SendByte(uint8_t value)
 {
-    for (int8_t i = 7; i >= 0; i--)
+    uint8_t mask = 0x80;
+
+    while (mask != 0)
     {
-        if ((data >> i) & 0x01)
+        if ((value & mask) != 0)
         {
-            SendHighBit();
+            LED_HighBit();
         }
         else
         {
-            SendLowBit();
+            LED_LowBit();
         }
+
+        mask >>= 1;
     }
 }
 
@@ -70,16 +84,24 @@ void LED_Init(void)
 
     __HAL_RCC_GPIOA_CLK_ENABLE();
 
-    GPIO_InitStruct.Pin = LED_H_PIN | LED_L_PIN;
+    /*
+     * Ausgangspegel vor der GPIO-Initialisierung setzen:
+     * DIN_H = LOW
+     * DIN_L = HIGH
+     */
+    DIN_H_LOW();
+    DIN_L_HIGH();
+
+    GPIO_InitStruct.Pin = DIN_H_PIN | DIN_L_PIN;
     GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
     GPIO_InitStruct.Pull = GPIO_NOPULL;
-    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
 
     HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
-    DIN_Middle();
+    LED_Middle();
 
-    HAL_Delay(5);
+    HAL_Delay(10);
 }
 
 void LED_SetColor(uint8_t index,
@@ -99,12 +121,26 @@ void LED_SetColor(uint8_t index,
 
 void LED_Show(void)
 {
+    __disable_irq();
+
     for (uint8_t i = 0; i < LED_COUNT; i++)
     {
+        /*
+         * OST4ML8132A erwartet BGR:
+         * Blau, Grün, Rot
+         */
         LED_SendByte(leds[i].b);
         LED_SendByte(leds[i].g);
         LED_SendByte(leds[i].r);
     }
 
-    HAL_Delay(5);
+    LED_Middle();
+
+    __enable_irq();
+
+    /*
+     * Set-/Latch-Zeit.
+     * Das Arduino-Beispiel verwendet 1 ms.
+     */
+    HAL_Delay(1);
 }
