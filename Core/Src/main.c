4bd66db02c987f7c8cbae7d18e00d2cc56b3ba66
johnnyeatworld89 <johnnyeatworld89@gmail.com>
2026-09-21 17:51:21 +0900

    Update main.c

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -234,6 +234,7 @@ static void MX_GPIO_Init(void)
     /* Enable GPIO clocks */
     __HAL_RCC_GPIOA_CLK_ENABLE();
     __HAL_RCC_GPIOB_CLK_ENABLE();
+    __HAL_RCC_GPIOC_CLK_ENABLE();
 
 
     /* ---------------------------------------------------------------------- */
@@ -304,6 +305,43 @@ static void MX_GPIO_Init(void)
 
     HAL_GPIO_Init(
         GPIOA,
+        &GPIO_InitStruct
+    );
+
+    /* ---------------------------------------------------------------------- */
+    /* Encoder                                                                 */
+    /* PA0 = SW                                                                */
+    /* PA1 = B                                                                 */
+    /* PC1 = A                                                                 */
+    /* ---------------------------------------------------------------------- */
+
+    GPIO_InitStruct.Pin =
+        GPIO_PIN_0 |
+        GPIO_PIN_1;
+
+    GPIO_InitStruct.Mode =
+        GPIO_MODE_INPUT;
+
+    GPIO_InitStruct.Pull =
+        GPIO_PULLUP;
+
+    HAL_GPIO_Init(
+        GPIOA,
+        &GPIO_InitStruct
+    );
+
+
+    GPIO_InitStruct.Pin =
+        GPIO_PIN_1;
+
+    GPIO_InitStruct.Mode =
+        GPIO_MODE_INPUT;
+
+    GPIO_InitStruct.Pull =
+        GPIO_PULLUP;
+
+    HAL_GPIO_Init(
+        GPIOC,
         &GPIO_InitStruct
     );
 }
